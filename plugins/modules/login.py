d2ea2b8aa4f1e8b045b12e8aec19195ae9f59b29
Frank Henninger <fhenninger@sertifi.com>
2023-07-31 16:22:45 +0000

    Issue-192: Add skip_password_reset

--- plugins/modules/login.py
+++ plugins/modules/login.py
@@ -61,6 +61,11 @@ options:
     type: str
     required: false
     version_added: '2.0.1'
+  skip_password_reset:
+    description:
+      - Skips the password reset if the login exists and I(password) is set.
+    type: bool
+    required: false
 author: "John McCall (@lowlydba)"
 notes:
   - Module will always return changed if a password is supplied.
